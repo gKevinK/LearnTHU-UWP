6cfb8b3068f77f5144eb392e1a81556631add5ba
Kevin <gkangkai@gmail.com>
2016-01-27 00:17:10 +0800

    Add LoginNewAsync()

--- Learn.THU/Model/Web.cs
+++ Learn.THU/Model/Web.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace LearnTHU.Model
 {
@@ -69,40 +71,86 @@ namespace LearnTHU.Model
         {
             try
             {
-                string url1 = @"https://learn.cic.tsinghua.edu.cn/index";
+                string url1 = @"http://learn.cic.tsinghua.edu.cn/index";
                 HttpWebRequest req1 = (HttpWebRequest)WebRequest.Create(url1);
+                req1.CookieContainer = cc;
                 HttpWebResponse res1 = (HttpWebResponse)await req1.GetResponseAsync();
                 string html1 = await new StreamReader(res1.GetResponseStream()).ReadToEndAsync();
+                cc.Add(new Uri(@"http://learn.cic.tsinghua.edu.cn"), res1.Cookies);
                 res1.Dispose();
                 Regex reg = new Regex(@"method=""post"" action=(\S+?)>");
                 string url2 = reg.Match(html1).Groups[1].Value;
-
-                HttpWebRequest req2 = (HttpWebRequest)WebRequest.Create(url2);
-                req2.Method = "POST";
-                string form = String.Format(@"i_user={0}&i_pass={1}",
-                    WebUtility.UrlEncode(userId), WebUtility.UrlEncode(userPwd));
-                req2.ContentType = @"application/x-www-form-urlencoded";
-                req2.CookieContainer = cc;
-                using (Stream requestStream = await req2.GetRequestStreamAsync())
+                
+                HttpClientHandler hch = new HttpClientHandler();
+                hch.AllowAutoRedirect = false;
+                hch.CookieContainer = cc;
+                hch.UseCookies = true;
+                HttpClient hc = new HttpClient(hch);
+                string form = String.Format(@"i_user={0}&i_pass={1}", userId, WebUtility.UrlEncode(userPwd));
+                StringContent sc = new StringContent(form);
+                sc.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/x-www-form-urlencoded");
+                HttpResponseMessage hrm = await hc.PostAsync(url2, sc);
+                if (hrm.StatusCode != HttpStatusCode.Redirect && hrm.StatusCode != HttpStatusCode.MovedPermanently)
                 {
-                    StreamWriter requestStreamWriter = new StreamWriter(requestStream);
-                    requestStreamWriter.Write(form);
-                    requestStreamWriter.Flush();
-                    requestStream.Flush();
-                    requestStreamWriter.Dispose();
+                    return LoginResult.Error;
                 }
-                HttpWebResponse res2 = (HttpWebResponse)await req2.GetResponseAsync();
+                string url3 = hrm.Headers.Location.ToString();
+                if (url3.Contains(@"SUCCESS"))
+                {
+                    await Request(url3);
+                    return LoginResult.Success;
+                }
+                else
+                {
+                    return LoginResult.Failed;
+                }
             }
             catch
             {
                 return LoginResult.Error;
             }
-            return LoginResult.Success;
         }
 
         public enum LoginResult
         {
             Success, Failed, Error
+        }
+
+        public async Task<string> Request(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.CookieContainer = cc;
+            HttpWebResponse res = (HttpWebResponse)await req.GetResponseAsync();
+            string html;
+            if (res.ContentLength == -1)
+            {
+                html = await ReadAllAsync(res.GetResponseStream());
+            }
+            else
+            {
+                html = await new StreamReader(res.GetResponseStream()).ReadToEndAsync();
+            }
+            if (res.Cookies.Count > 0)
+            {
+                cc.Add(new Uri(@"http://" + new Uri(url).Host), res.Cookies);
+            }
+            res.Dispose();
+            return html;
+        }
+
+        private async Task<string> ReadAllAsync(Stream stream)
+        {
+            byte[] buf = new byte[4096];
+            Stream ms = new MemoryStream();
+            int count = await stream.ReadAsync(buf, 0, 4096);
+            while (count > 0)
+            {
+                ms.Write(buf, 0, count);
+                count = stream.Read(buf, 0, 4096);
+            }
+            ms.Seek(0, SeekOrigin.Begin);
+            string a = new StreamReader(ms, Encoding.UTF8).ReadToEnd();
+            return a;
         }
     }
 }
